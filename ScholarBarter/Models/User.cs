eebbfcdfcbae485ece4e4f3e5b1887ce56d6fb4e
Arun Singh <arunsinghit@gmail.com>
2016-01-19 12:25:05 -0800

    Added WebAPi model and controller

---
+++ ScholarBarter/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScholarBarter.Models
+{
+  public class User
+  {
+    public int ID { get; set; }
+    public string userName { get; set; }
+    public string userAddress { get; set; }
+    public string phone { get; set; }
+
+  }
+}
