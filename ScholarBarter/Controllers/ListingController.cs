dd017d529511874a13661d29b21a9e4bd13f402d
Kshitija Shinde <shinde.k888@gmail.com>
2016-01-20 14:00:34 -0800

    Added sample web api to get list

---
+++ ScholarBarter/Controllers/ListingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ScholarBarter.Models;
+
+namespace ScholarBarter.Controllers
+{
+   
+    public class ListingController : ApiController
+    {
+      private Listing[] listings = new Listing[]
+      {
+        new Listing {ID = 1, ListName = "CS580 tutoring", description = "test1234 ....", Date = "1/1/16"},
+        new Listing {ID = 2, ListName = "CS334 tutoring", description = "test1234 ....", Date = "1/1/16"},
+        new Listing {ID = 3, ListName = "Math Tutoring", description = "test1234 ....", Date = "1/1/16"}
+      };
+
+      public IEnumerable<Listing> GetAllListing()
+      {
+        return listings;
+      }
+
+    }
+}
