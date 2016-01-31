551eacf096fdb461e167b532b34c9a050eacc4f7
Dominick Atanasio <dom@domsnet.net>
2016-01-31 00:15:14 -0800

    I removed the first email library that I had installed from NuGet and added Simplify.Mail. I created an email class and an email test controller (this will be removed at a later date)

---
+++ ScholarBarter/Controllers/TestEmailController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ScholarBarter.Models;
+
+namespace ScholarBarter.Controllers
+{
+    public class TestEmailController : ApiController
+    {
+        [HttpPost]
+        public void testEmail()
+        {
+            Email e = new Email
+            {
+                From = "support@scholarbarter.com",
+                To = "dom@domsnet.net",
+                Subject = "This is a test",
+                Body = "<h2>Testing</h2>"
+            };
+
+            e.send();
+        }
+    }
+}
---
+++ ScholarBarter/Models/Email.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Simplify.Mail;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace ScholarBarter.Models
+{
+    public class Email
+    {
+        public string From { set; get; }
+        public string To { set; get; }
+        public string Subject { set; get; }
+        public string Body { set; get; }
+
+        public void send()
+        {
+            if (isValid)
+            {
+                MailSender sndr = new MailSender();
+                sndr.Send(From, To, Subject, Body);
+            }
+            else
+            {
+                throw new Exception("The email is not properly formated");
+            }
+        }
+
+        private bool isValid
+        {
+            get
+            {
+                var validator = new EmailAddressAttribute();
+
+                return (validator.IsValid(From) && validator.IsValid(To));
+            }
+        }
+    }
+}
