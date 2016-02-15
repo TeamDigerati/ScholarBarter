67980aa14c3095706b829a466cbde6430fb415e3
Dominick Atanasio <dom@domsnet.net>
2016-02-14 19:27:21 -0800

    configures QL server for remote connection for dev added a user table

---
+++ ScholarBarter/Sql/Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]       INT           IDENTITY (1, 1) NOT NULL,
+    [Username]     VARCHAR (50)  NOT NULL,
+    [EduEmail]     VARCHAR (150) NOT NULL,
+    [Email]        VARCHAR (150) NOT NULL,
+    [PasswordHash] VARCHAR (MAX) NOT NULL,
+    [Enabled]      BIT           CONSTRAINT [DF_Users_Enabled] DEFAULT ((0)) NOT NULL,
+    [CreationTime] DATETIME      CONSTRAINT [DF_Users_CreationTime] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
