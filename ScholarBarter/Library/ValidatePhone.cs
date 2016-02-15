a8f6ea493265091053ee8f45ebfcea355a6511c2
Kshitija Shinde <shinde.k888@gmail.com>
2016-02-14 20:33:12 -0800

    Added unit test case for validating phone number

---
+++ ScholarBarter/Library/ValidatePhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+namespace ScholarBarter.Library
+{
+    public class ValidatePhone
+    {
+        public  bool IsPhoneNumber(string number)
+        {
+            string MatchPhoneNumberPattern = "^\\(?([0-9]{3})\\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+   if (number!= null) return Regex.IsMatch(number, MatchPhoneNumberPattern );
+   else return false;
+        }
+    }
+}
---
+++ ScholarBarterTests/Models/ValidatePhoneTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScholarBarter.Library;
+
+
+namespace ScholarBarterTests.Models.Test
+{
+    [TestClass]
+    public class ValidatePhoneTest
+    {
+        [TestMethod]
+        public void ValidPhoneTest()
+        {
+         ValidatePhone util = new ValidatePhone();
+         string phonenumber = "3114567777";
+         Assert.IsTrue(util.IsPhoneNumber(phonenumber));
+         }
+        [TestMethod]
+        public void inValidPhoneTest()
+        {
+        ValidatePhone util = new ValidatePhone();
+         string phonenumber = "3A114567777";
+         Assert.IsFalse(util.IsPhoneNumber(phonenumber));
+         }
+
+    }
+}
