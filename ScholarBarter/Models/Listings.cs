a6bc2d2264aacbc2d2c597cde5965bb9c04b4711
Arun Singh <arunsinghit@gmail.com>
2016-02-25 15:41:52 -0800

    Added css, images and base model

---
+++ ScholarBarter/Models/Listings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScholarBarter.Models
+{
+    public class Listings 
+    {
+
+      public int Id { get; set; }
+      [StringLength(50)]
+      public string Product { get; set; }
+      public decimal Price { get; set; }
+      public int Quantity { get; set; }
+      public DateTime Date { get; set; }
+      public int CustomerId { get; set; }
+      public User User { get; set; }
+
+      public Listings Clone()
+      {
+        return (Listings)this.MemberwiseClone();
+      }
+
+    }
+}
--- ScholarBarter/Models/User.cs
+++ ScholarBarter/Models/User.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace ScholarBarter.Models
 {
   public class User
   {
-    public int ID { get; set; }
-    public string userName { get; set; }
-    public string userAddress { get; set; }
-    public string phone { get; set; }
+    public int Id { get; set; }
+    [StringLength(50)]
+    public string FirstName { get; set; }
+    [StringLength(50)]
+    public string LastName { get; set; }
+    [StringLength(100)]
+    public string Email { get; set; }
+    [StringLength(1000)]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public Gender Gender { get; set; }
+    public ICollection<Listings> Listings { get; set; }
 
+  }
+  public enum Gender
+  {
+    Female,
+    Male
   }
 }
